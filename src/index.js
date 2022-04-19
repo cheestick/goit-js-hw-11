c59c3108a4f68073a887d868ef7754931bdfbb42
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-04-19 20:12:15 +0300

    code garbage

--- src/index.js
+++ src/index.js
@@ -4,24 +4,60 @@ import refs from './js/refs';
 import * as APIService from './js/APIService';
 import * as View from './js/View';
 import * as LoadMore from './js/loadMore';
+import QueryController from './js/QueryController';
 import './sass/main.scss';
 
 LoadMore.hide();
+
+const slider = new simpleLightbox('.gallery a');
+
 refs.searchForm.addEventListener('submit', onSearchFormSubmit);
 
 async function onSearchFormSubmit(event) {
   event.preventDefault();
-
-  const searchQuery = event.target.elements.searchQuery.value;
+  const searchQuery = event.target.elements.searchQuery.value.trim();
+  if (!searchQuery) return;
+  QueryController.queryString = searchQuery;
+  if (!QueryController.isSearchNew()) {
+    return;
+  } else {
+    View.clearGallery(refs.gallery);
+  }
 
   try {
     const response = await APIService.fetchPhotos(searchQuery);
     View.notifySearchQuery(response);
+
+    const { totalHits, hits } = response;
+    const pagesQuantity = Math.floor(totalHits / hits.length);
+    QueryController.init(searchQuery, pagesQuantity);
+
     View.renderGallery(refs.gallery, response.hits);
-    new simpleLightbox('.gallery a');
+    slider.refresh();
     LoadMore.show();
+    LoadMore.addClickListener(onClickMore);
   } catch (error) {
     event.target.reset();
+    return;
+  }
+}
+
+async function onClickMore(event) {
+  try {
+    console.log(QueryController.page);
+    const page = QueryController.nextPage;
+    console.log(QueryController.page);
+    const searchString = QueryController.queryString;
+    const response = await APIService.fetchPhotos(searchString, page);
+    View.renderGallery(refs.gallery, response.hits);
+    slider.refresh();
+  } catch (error) {
+    return;
+  }
+
+  if (!QueryController.hasNextPage()) {
+    View.notifyEndOfGallery();
+    LoadMore.hide();
     return;
   }
 }
